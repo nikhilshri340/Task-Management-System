825712b118e620edd15f7d285169c2ae1b282af4
nikhilshri340 <nikhilshrivastava340@gmail.com>
2026-05-16 17:15:21 +0530

    Updated frontend API URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "http://localhost:5000/api",
+    "https://task-management-system-production-450f.up.railway.app/api",
 });
 
 export default api;
