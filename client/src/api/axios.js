c205c3dc1bf32bc7236a8d0b7e92dbf7e908eeee
nikhilshri340 <nikhilshrivastava340@gmail.com>
2026-05-16 12:26:19 +0530

    Fix auth controller ES modules

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "task-management-system-production-450f.up.railway.app/api",
+  baseURL: "https://task-management-system-production-450f.up.railway.app/api",
 });
 
 /*
